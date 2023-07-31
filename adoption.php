e303626804d89e657e98f75d26d9ccc39cb7e795
JanA867 <jsc.simonis@gmail.com>
2023-07-31 15:13:43 +0200

    Adoption

--- adoption.php
+++ adoption.php
@@ -4,40 +4,51 @@ session_start();
 
 require_once  "db_connection.php";
 
-if(!isset($_SESSION["user"]) && !isset($_SESSION["adm"])){ //if not session user AND not session adm > send to login page
+if(!isset($_SESSION["user"]) && !isset($_SESSION["adm"])){ 
     header("Location: login.php");
        }
 
-//Animal Id
-$id=$_GET["x"];
+//Animal Id über Pos + Value
+$pet_id=$_GET["x"];
+//User Id über Session
+$new_owner_id= $_SESSION["user"];
 
-$sqlA="SELECT*FROM animals WHERE id=$id";
-$resultA=mysqli_query($connect,$sqlA);
-$rowA=mysqli_fetch_assoc($resultA);
-
-//User Id
-$sqlU="SELECT*FROM user WHERE id=$id";
-$resultU=mysqli_query($connect,$sqlU);
-$rowU=mysqli_fetch_assoc($resultU);
-
-$new_owner_id= $_SESSION["user"] = $rowU["id"];
-
-
-if(isset($_POST["adopt"])){
-    $pet_id= $_POST["id"];
-    $new_owner_id = $POST["id"];
-
-$sql= "INSERT INTO `pet_adoption`(NEW_OWNER_ID, PET_ID) VALUES ($new_owner_id, $pet_id)";
-
-if(mysqli_query($connect, $sql)){
-    echo "<div class='alert alert-success' role='alert'>
-    New record has been created, {$picture[1]}
-  </div>";
-  header("refresh: 3; url=index.php");
-}else{
-    echo "<div class='alert alert-danger' role='alert'>
-    Error found,  {$picture[1]}!
-  </div>";
-
+$result= mysqli_query($connect, "SELECT status FROM animals WHERE id=$pet_id"); //ob animal adoted
+$available= mysqli_fetch_assoc($result); //holt Daten für check 
+if($available["status"] === "available"){ //vergleicht ob available
+  $sql= "INSERT INTO `pet_adoption`(NEW_OWNER_ID, PET_ID, DATE) VALUES ($new_owner_id, $pet_id, NOW())"; //neuer Eintrag in DB
+  
+  if(mysqli_query($connect, $sql)){ //wenn erfolgreich:  echo
+      echo "<div class='alert alert-success' role='alert'>Successfully adopted
+    </div>";
+    mysqli_query($connect, "UPDATE `animals` SET `status`='adopted' WHERE id = $pet_id"); //wenn erfolgreich: update status
+    header("refresh: 3; url=home.php"); //leite weiter
+  }else{
+      echo "<div class='alert alert-danger' role='alert'>Error
+    </div>";
+    header("refresh: 3; url=home.php");
+  }
 }
+else{ //wenn schon adotiert: echo
+  echo "<div class='alert alert-danger' role='alert'>Already adopted 
+    </div>";
+    header("refresh: 3; url=home.php");
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+    <meta charset="UTF-8">
+    <meta  name="viewport"  content="width=device-width, initial-scale=1.0" >
+   <title>Dashboard <?= $row["first_name"] ?></title>
+    <link href ="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"  rel= "stylesheet" integrity ="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"  crossorigin= "anonymous">
+
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
